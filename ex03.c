72cae804aec6d29d20dde64d07d2a2c656d05588
Keila Alves <50184400+Keilaalves1803@users.noreply.github.com>
2024-06-24 20:01:12 -0300

    Update ex03.c

--- ex03.c
+++ ex03.c
@@ -6,14 +6,14 @@ int main(){
 	
 	float n1, n2, media;
 	
-	printf("Digite -> n1:n2\n");
-	scanf("%f:%f", &n1, &n2);
+	printf("Digite -> n1:n2\n"); // indicando como deve ser digitada a entrada de ambas variaveis pelo usuario
+	scanf("%f:%f", &n1, &n2); //definindo entrada das variaveis
 	media = (n1+n2) / 2;
-	printf("\nSua media: %.2f", media);
+	printf("\nSua media: %.2f", media); //limitando quantidade de algarismos após virgula
 	
-	setlocale(LC_ALL, "Portuguese");
+	setlocale(LC_ALL, "Portuguese"); // funcao para aceitar caracteres em portugues
 	
-	if(media >= 6.0) printf("\nAprovado!!");
+	if(media >= 6.0) printf("\nAprovado!!"); //if aceita o nao uso de colchetes dessa forma
 	if(media < 4.0) printf("\nReprovado!!");
 	if(media < 6.0 && media > 4.0) printf("\nRecuperacao!!");
 	
