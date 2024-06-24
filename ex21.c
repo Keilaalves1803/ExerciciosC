5e5e69aeb5616d282ae610aae402191fb0733a2b
Keila Alves <50184400+Keilaalves1803@users.noreply.github.com>
2024-06-24 20:52:30 -0300

    Create ex21.c

---
+++ ex21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int matriz[5][3], i, j;
+	for(i = 0; i < 3; i++){
+		for(j = 0; j < 3; j++){	
+			scanf("%i", &matriz[i][j]);
+		}
+	}
+	for(i = 0; i < 3; i++){
+		for(j = 0; j < 3; j++){	
+				printf("%d ", matriz[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
