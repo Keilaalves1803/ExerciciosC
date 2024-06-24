24960a8dad5aa74e509792fa5348d91c17ec06ec
Keila Alves <50184400+Keilaalves1803@users.noreply.github.com>
2024-06-24 20:46:04 -0300

    Add files via upload

---
+++ ex20/operacoes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "operacoes.h"
+
+float soma(float a, float b){
+	return a + b;
+}
+float sub(float a, float b){
+	return a - b;
+}
+float media(float a, float b){
+	return (a + b) / 2;
+}
---
+++ ex20/operacoes.h
@@ -0,0 +1,3 @@
+float media(float a, float b);
+float soma(float a, float b);
+float sub(float a, float b);
