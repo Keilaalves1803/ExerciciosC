cdd5ccf3c9722e8e6c5cab9a0635381c34bcfe54
Keila Alves <50184400+Keilaalves1803@users.noreply.github.com>
2024-06-24 20:02:00 -0300

    Update and rename ex05.cpp to ex05.c

--- ex05.c
+++ ex05.c
@@ -9,21 +9,21 @@ int main(){
 	setlocale(LC_ALL, "Portuguese");
 	printf("Digite o valor salarial: R$");
 	scanf("%f", &salario);
-	printf("Digite a categoria profissional. \nT/t - TÈcnico. \nG/g - Gerente. \nO/o - Demais categorias.\n");
+	printf("Digite a categoria profissional. \nT/t - T√©cnico. \nG/g - Gerente. \nO/o - Demais categorias.\n");
 	scanf(" %c", &tipo);
 	
 	
 	if(tipo == 'T' || tipo == 't'){
 		salarioReaj = salario * 1.5;
-		printf ("O sal·rio com reajuste È de: R$%.2f", salarioReaj);
+		printf ("O sal√°rio com reajuste √© de: R$%.2f", salarioReaj);
 	}else if(tipo == 'G' || tipo == 'g'){
 		salarioReaj = salario * 1.3;
-		printf ("O sal·rio com reajuste È de: R$%.2f", salarioReaj);
+		printf ("O sal√°rio com reajuste √© de: R$%.2f", salarioReaj);
 	}else if(tipo == 'O' || tipo =='o'){
 		salarioReaj = salario * 1.2;
-		printf ("O sal·rio com reajuste È de: R$%.2f", salarioReaj);
+		printf ("O sal√°rio com reajuste √© de: R$%.2f", salarioReaj);
 	}else{
-		printf ("Tipo inv·lido");
+		printf ("Tipo inv√°lido");
 	}
 	
 	return 0;
